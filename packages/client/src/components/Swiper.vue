b33bb93d129996802346f4c655ff9a79617e1c6b
catxrine <catxrin@gmail.com>
2025-01-13 16:44:45 +0200

    wip

--- packages/client/src/components/Swiper.vue
+++ packages/client/src/components/Swiper.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue';
 import { Navigation,Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide,useSwiper } from 'swiper/vue';
 
@@ -8,21 +7,11 @@ import artist2 from "../public/angelina-litvin-52R7t7x8CPI-unsplash.jpg"
 import artist4 from "../public/luke-braswell-oYFv-_JKsVk-unsplash.jpg"
 import artist1 from "../public/swapnil-dwivedi-N2IJ31xZ_ks-unsplash.jpg"
 
-import Icon from './Icon.vue';
-
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
-const slider = ref(null);
-    const swiper= useSwiper()
-        const onSwiper = (swiper) => {
-          slider.value=swiper.value
-        };
-        const onSlideChange = (e) => {
-          // console.log(e, swiper);
-        };
- 
-
+import 'swiper/css/navigation';
+   
 const artists = [{
   image:artist3,
   name:"Aiony Brat",
@@ -43,60 +32,75 @@ const artists = [{
 },
 ]
 
+const limitedCharacters = (text)=>{
+let characters = text
+if(characters.length>120){
+characters=characters.split("").slice(0,120).join("") +'...'
+}
+return characters
+}
+
     </script>
 <template>
-  <!-- <div class="min-h-full w-screen bg-gradient-to-l from-pink-300  to-pink-50" /> -->
   <Swiper
     :breakpoints="{500:{slidesPerView:1}, 800:{slidesPerView:2, spaceBetween:60}, 1200:{slidesPerView:3}, 1600:{slidesPerView:4}}"
-    allow-slide-next="true"
     :space-between="120"
+    nextButton= '.swiper-button-next'
+    :navigation="true"
+    prevButton='.swiper-button-prev'
     :pagination="{
       clickable: true,
     }"
     :modules="[Pagination, Navigation]"
-    class="min-h-[35rem] !w-screen bg-gradient-to-l from-pink-300  to-pink-50 !px-28 !py-10 md:!px-14"
+    class="min-h-[35rem] !w-screen bg-gradient-to-l flex flex-row from-pink-300  to-pink-50 !px-10 !py-10 md:!px-14"
   >
+ 
     <SwiperSlide
       v-for="artist in artists"
       :key="artist"
-      class="!flex max-h-[30rem] flex-col !justify-between rounded-lg border border-pink-200 bg-white py-5"
+      class="!flex sm:max-h-[30rem] max-h-[26rem] flex-col !justify-between rounded-lg border border-pink-200 bg-white py-5"
     >
-      <!-- <div class="flex min-h-full w-80 flex-col justify-between rounded-lg border border-pink-200 bg-white py-2"> -->
       <div class="flex flex-col items-center gap-2">
-        <div class="size-52 rounded-lg p-2 shadow-[inset_0px_0px_37px_50px_rgba(236,_72,_153,_0.15)]">
+        <div class="sm:size-52 size-44 rounded-lg p-2 shadow-[inset_0px_0px_37px_50px_rgba(236,_72,_153,_0.15)]">
           <img
             :src="artist.image"
             alt="image"
-            class="size-full rounded object-cover"
+            class="sm:size-full rounded object-cover size-40"
           >
         </div>
         <div class="flex flex-col items-center gap-2 px-4">
           <div class="text-center">
             <p class="text-lg font-semibold">
               {{ artist.name }}
-            </p> <p class="text-sm text-gray-500">
+            </p> 
+            <p class="text-sm text-gray-500">
               5 years of experience
             </p>
           </div>
-          <p class="text-md pb-2 text-center">
-            {{ artist.describtion }}
+          <p class="sm:text-md text-sm pb-2 text-center">
+            {{ limitedCharacters(artist.describtion) }}
           </p>
         </div>
       </div>
       <div class="my-2 flex flex-row justify-center gap-6">
-        <button class="btn-outlined">
+        <button class="btn-outlined sm:text-md text-sm">
           Book me
         </button>
-        <button class="btn-outlined">
+        <button class="btn-outlined sm:text-md text-sm">
           See more
         </button>
+      
       </div>
     </SwiperSlide>
+    <div class="swiper-button-prev swiper-button"></div>
+    <div class="swiper-button-next swiper-button"></div>
   </Swiper>
-  <!-- </div> -->
   <div class="min-h-1 w-screen bg-gradient-to-r from-pink-300  to-pink-50" />
 </template>
 
-  
 
-
+<style lang="css" scoped>
+.swiper-button{
+  @apply bg-pink-200 rounded-full !text-pink-400 !fill-pink-400 !text-sm py-4 px-5 font-bold
+}
+</style>
