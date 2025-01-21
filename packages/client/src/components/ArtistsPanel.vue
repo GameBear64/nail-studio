cbcd9cc266867f4978e9e4800c1c7714272f850a
catxrin <catxrin@gmail.com>
2025-01-21 07:25:41 +0200

    started with the implementation of the Artists Management Table

--- packages/client/src/components/ArtistsPanel.vue
+++ packages/client/src/components/ArtistsPanel.vue
@@ -2,6 +2,34 @@
 
 <template>
   <div class="w-full">
-    artists panel
+    <table class="w-full">
+      <tr class="rounded bg-gray-100  text-left">
+        <th class="">
+          Name
+        </th>
+        <th>Phone</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th>Actions</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+    </table> 
   </div>
 </template>
+
+<style lang="css" scoped>
+
+
+th{
+    @apply font-light !min-w-32 px-2 py-3 uppercase
+}
+</style>
